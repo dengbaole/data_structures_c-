c43ecabe25a741aaf5940103ed4a6681a4ea628b
dengbaole <dengbaole155@gmail.com>
2025-03-05 16:31:21 +0800

    单链表

--- main.cpp
+++ main.cpp
@@ -27,31 +27,69 @@ listnode::listnode(int d) {
 }
 
 listnode* listinit(void) {
-    listnode *head = new listnode(1);
+    listnode *head = new listnode(0);
     return head;
 } 
 
 
 void headinsert(listnode *head, int data) {
     listnode *newnode = new listnode(data);
-    newnode->next = head;
-    head = newnode;
+    newnode -> next = head -> next;
+    head -> next = newnode;
+    head -> data++;
+}
+
+
+void tailinsert(listnode *head, int data) {
+    listnode *newnode = new listnode(data);
+    listnode *p = head;
+    while (p -> next != nullptr) {
+        p = p -> next;
+    }
+    p -> next = newnode;
+    head -> data++;
+}
+
+
+void deletenode(listnode *head, int data) {
+    listnode *p = head;
+    while (p -> next != nullptr) {
+        if (p -> next -> data == data) {
+            listnode *temp = p -> next;
+            p -> next = p -> next -> next;
+            delete temp;
+            head -> data--;
+            return;
+        } else {
+            p = p -> next;
+        }   
+    }
+}
+
+
+void printlist(listnode *head) {
+    listnode *p = head -> next;
+    while (p != nullptr) {
+        cout << p -> data << " ";
+        p = p -> next;
+    }
+    cout << endl;
 }
 
 int main() {
-    listnode *head = new listnode(1);
-    listnode *p = head;
-    for (int i = 2; i <= 5; ++i) {
-        p->next = new listnode(i);
-        p = p->next;
-    }
-
-    // 打印链表
-    p = head;
-    while (p != nullptr) {
-        cout << p->data << " ";
-        p = p->next;
-    }
+    listnode *head = listinit();
+    headinsert(head, 1);
+    headinsert(head, 2);
+    headinsert(head, 3);
+    headinsert(head, 3);
+    headinsert(head, 3);
+    headinsert(head, 3);
+    headinsert(head, 4);
+    headinsert(head, 5);
+    headinsert(head, 6);
+    tailinsert(head, 7);
+    deletenode(head, 3);
+    printlist(head);
 
     return 0;
 }

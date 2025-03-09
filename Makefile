4909ed3485d9bf3ec2f8cdc733771890782f1c11
dengbaole <dengbaole155@gmail.com>
2025-03-09 22:29:33 +0800

    更新哈希表

--- Makefile
+++ Makefile
@@ -1,19 +1,25 @@
-project_name := main
-uvproj_path := .
-.PHONY: default build cformat format clean flash debug assets
+# Makefile
 
-build:
-	@echo 【START】----- UV build -----
-	@g++ main.cpp -o main; .\main.exe
-	@echo 【 END 】----- UV build -----
+# 定义编译器和编译选项
+CXX = g++
+CXXFLAGS = -Wall -g
 
+# 定义目标文件和可执行文件
+TARGET = main.exe
+SOURCE = main.cpp
 
+# 默认目标
+.PHONY: all
+all: build
 
+# 编译目标
+.PHONY: build
+build: $(SOURCE)
+	$(CXX) $(CXXFLAGS) $(SOURCE) -o $(TARGET)
+	./$(TARGET)
 
-format:
-	./utils_bin/astyle.exe --project="./utils_bin/.astylerc" -r **.c,**.h --ignore-exclude-errors --exclude=_build --exclude=utils_bin --exclude=dist --exclude=utils -v -Q
-
-assets:
-	@echo Create Assets
-	./utils_bin/p2a-win-amd64.exe -in ./moudle/ui -out ./moudle/image
+# 清理目标
+.PHONY: clean
+clean:
+	rm -f $(TARGET)
 
--- main.cpp
+++ main.cpp
@@ -9,90 +9,89 @@ using namespace std;
 
 struct Pair {
     public:
-    int key;
-    string val;
-    Pair(int k, string v) : key(k), val(v) {}
+        int key;
+        string val;
+        Pair(int k, string v) : key(k), val(v) {}
 };
 
 
 
 class ArrayHashTable {
     private:
-     vector<Pair*> tables;
+        vector<Pair*> tables;
     public:
-    ArrayHashTable() {
-        tables = vector<Pair*>(100);
-    }
-    ~ArrayHashTable() {
-        for (auto pair : tables) {
-            delete pair;
+        ArrayHashTable() {
+            tables = vector<Pair*>(100);
         }
-        tables.clear();
-    }
-
-    int hashFunc(int key){
-        return key % tables.size();
-    }
-
-    string get(int key) {
-        int index = hashFunc(key);
-        Pair* pair = tables[index];
-        if(pair == nullptr) {
-            return "'";
+        ~ArrayHashTable() {
+            for (auto pair : tables) {
+                delete pair;
+            }
+            tables.clear();
         }
-        return pair->val;
-    }
 
-    void put(int key, string val) {
-        Pair* pair = new Pair(key, val);
-        int index = hashFunc(key);
-        tables[index] = pair;
-    }
+        int hashFunc(int key){
+            return key % tables.size();
+        }
 
-    void remove (int key) {
-        int index = hashFunc(key);
-        delete tables[index];
-        tables[index] = nullptr;
-    }
+        string get(int key) {
+            int index = hashFunc(key);
+            Pair* pair = tables[index];
+            if(pair == nullptr) {
+                return "'";
+            }
+            return pair->val;
+        }
 
-    vector<Pair*> pairset() {
-        vector<Pair*> pairset;
-        for (auto pair : tables) {
-            if(pair != nullptr) {
-                pairset.push_back(pair);
+        void put(int key, string val) {
+            Pair* pair = new Pair(key, val);
+            int index = hashFunc(key);
+            tables[index] = pair;
+        }
+
+        void remove (int key) {
+            int index = hashFunc(key);
+            delete tables[index];
+            tables[index] = nullptr;
+        }
+
+        vector<Pair*> pairset() {
+            vector<Pair*> pairset;
+            for (auto pair : tables) {
+                if(pair != nullptr) {
+                    pairset.push_back(pair);
+                }
+            }
+            return pairset;
+        }
+
+        vector<int> keyset() {
+            vector<int> keyset;
+            for (auto pair : tables) {
+                if(pair != nullptr) {
+                    keyset.push_back(pair->key);
+                }
+            }
+            return keyset;
+        }
+
+        vector<string> valueset() {
+            vector<string> valueset;
+            for (auto pair : tables) {
+                if(pair != nullptr) {
+                    valueset.push_back(pair->val);
+                }
+            }
+            return valueset;
+        }
+
+        void print() {
+            for (auto pair : tables) {
+                if(pair != nullptr) {
+                    cout << pair->key << " : " << pair->val << endl;
+                }
             }
         }
-        return pairset;
-    }
-
-    vector<int> keyset() {
-        vector<int> keyset;
-        for (auto pair : tables) {
-            if(pair != nullptr) {
-                keyset.push_back(pair->key);
-            }
-        }
-        return keyset;
-    }
-
-    vector<string> valueset() {
-        vector<string> valueset;
-        for (auto pair : tables) {
-            if(pair != nullptr) {
-                valueset.push_back(pair->val);
-            }
-        }
-        return valueset;
-    }
-
-    void print() {
-        for (auto pair : tables) {
-            if(pair != nullptr) {
-                cout << pair->key << " : " << pair->val << endl;
-            }
-        }
-    }
-
 };
 
 
@@ -115,9 +114,19 @@ int main() {
     cout << hashTable->get(5) << endl;
     cout << hashTable->get(6) << endl;
 
-    
+     hashTable->remove(3);
 
 
     hashTable->print();
+        // 使用 pairset() 方法获取所有有效的 Pair 对象
+    
+  // 使用 keyset() 方法获取所有有效的键
+    vector<int> allKeys = hashTable->keyset();
+
+    // 打印所有键
+    cout << "All keys in the hash table:" << endl;
+    for (auto key : allKeys) {
+        cout << key << endl;
+    }
     return 0;
 }
